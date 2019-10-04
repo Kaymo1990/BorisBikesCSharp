a89a307ee3b82fbd8d6b28bfbd67e1fc3c1fff60
Kay <Kay@LAPTOP-R26J0GK6>
2019-10-04 20:03:28 +0100

    updated with new project

---
+++ BorisBikes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BorisBikes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
